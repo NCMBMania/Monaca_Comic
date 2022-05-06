b3177c16d321ed59195152fb40a6844881fac071
Atsushi <atsushi@moongift.jp>
2022-05-06 18:56:12 +0900

    Init

--- www/js/routes.js
+++ www/js/routes.js
@@ -3,79 +3,16 @@ const routes = [
     path: '/',
     url: './index.html',
   },
+  // コミック一覧画面
   {
-    path: '/about/',
-    url: './pages/about.html',
+    path: '/comics/',
+    componentUrl: './pages/list.html',
   },
+  // コミック詳細画面
   {
-    path: '/form/',
-    url: './pages/form.html',
+    path: '/comics/:key',
+    componentUrl: './pages/view.html',
   },
-  {
-    path: '/catalog/',
-    componentUrl: './pages/catalog.html',
-  },
-  {
-    path: '/product/:id/',
-    componentUrl: './pages/product.html',
-  },
-  {
-    path: '/settings/',
-    url: './pages/settings.html',
-  },
-
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    componentUrl: './pages/dynamic-route.html',
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = to.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            componentUrl: './pages/request-and-load.html',
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
-    },
-  },
-  // Default route (404 page). MUST BE THE LAST
   {
     path: '(.*)',
     url: './pages/404.html',
